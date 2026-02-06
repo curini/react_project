84320c20655dfb44d02815572c8c9138cbffe7a9
LAPTOP-J9U9GBB8\curin <curini>
2026-02-06 14:44:28 +0100

    add Gallery component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import "./App.css";
+import Gallery from "./components/Gallery";
 
 function App() {
-  return <p>Hello, World!</p>;
+  return (
+    <div>
+      <p className="text-3xl font-bold underline">Hello, World!</p>
+      <Gallery />
+    </div>
+  );
 }
 
 export default App;
