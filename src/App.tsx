2603249fe9ed65e44a06fb62657ea1913fc10f55
LAPTOP-J9U9GBB8\curin <curini>
2026-02-09 16:26:31 +0100

    remove div

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,10 @@ import Gallery from "./components/Gallery";
 
 function App() {
   return (
-    <div>
+    <>
       <p className="text-3xl font-bold underline">Hello, World!</p>
       <Gallery />
-    </div>
+    </>
   );
 }
 
