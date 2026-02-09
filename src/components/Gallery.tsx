2b519f9e34648a79bc0cd3261211825572792ee0
LAPTOP-J9U9GBB8\curin <curini>
2026-02-09 15:45:36 +0100

    add props and type

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -1,14 +1,25 @@
-function Profile() {
-  return <img src="https://i.imgur.com/MK3eW3As.jpg" alt="Katherine Johnson" />;
+import type { JSX } from "react";
+
+type Person = {
+  name: string;
+  imageId: string;
+};
+
+function getImageUrl(person: Person): string {
+  return "https://i.imgur.com/" + person.imageId + "s.jpg";
 }
 
-export default function Gallery() {
+function Profile({ person }: { person: Person }): JSX.Element {
+  return <img src={getImageUrl(person)} alt={person.name} />;
+}
+
+export default function Gallery(): JSX.Element {
   return (
     <section>
       <h1>Amazing scientists</h1>
-      <Profile />
-      <Profile />
-      <Profile />
+      <Profile person={{ name: "Katsuko Saruhashi", imageId: "YfeOqp2" }} />
+      <Profile person={{ name: "Aklilu Lemma", imageId: "OKS67lh" }} />
+      <Profile person={{ name: "Lin Lanying", imageId: "1bX5QH6" }} />
     </section>
   );
 }
