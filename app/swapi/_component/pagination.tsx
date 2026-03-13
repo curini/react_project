1ac110bca58f428e3849e22a9a7f2ed0ababafdc
LAPTOP-J9U9GBB8\curin <curini>
2026-03-13 17:17:26 +0100

    add pagination component

---
+++ app/swapi/_component/pagination.tsx
@@ -0,0 +1,32 @@
+import Link from "next/link";
+
+export default function Pagination({
+  currentPage,
+  maxPage,
+  url,
+}: {
+  currentPage: number;
+  maxPage: number;
+  url: string;
+}) {
+  return (
+    <div className="mt-2 mb-2">
+      {Array.from({ length: maxPage }).map((value, index) => {
+        const realIndex = index + 1;
+        return realIndex == currentPage ? (
+          <span key={index} className="border px-2 py-2">
+            {realIndex}
+          </span>
+        ) : (
+          <Link
+            key={index}
+            className="border px-2 py-2 hover:bg-black hover:text-white"
+            href={url + "?page=" + realIndex}
+          >
+            {realIndex}
+          </Link>
+        );
+      })}
+    </div>
+  );
+}
