4e873adf18f46543413ba1a3c2df093c3c2f5f1c
LAPTOP-J9U9GBB8\curin <curini>
2026-03-19 16:05:52 +0100

    update navbar

--- app/_component/nav.tsx
+++ app/_component/nav.tsx
@@ -2,14 +2,18 @@ import Link from "next/link";
 
 export default function Nav() {
   const classNameLink = "hover:bg-white hover:text-black h-full py-2 px-1";
+  const links = [
+    { name: "Home", url: "/" },
+    { name: "Swapi", url: "/swapi" },
+    { name: "Contact", url: "/contact" },
+  ];
   return (
     <nav className="bg-black flex gap-4 text-white justify-center">
-      <Link href="/" className={classNameLink}>
-        Home
-      </Link>
-      <Link href="/swapi" className={classNameLink}>
-        Swapi
-      </Link>
+      {links.map((value, key) => (
+        <Link key={key} href={value.url} className={classNameLink}>
+          {value.name}
+        </Link>
+      ))}
     </nav>
   );
 }
