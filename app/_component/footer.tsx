51dfb70cdb52a8a1047664685443c5a3cf6b7501
LAPTOP-J9U9GBB8\curin <curini>
2026-03-19 16:04:54 +0100

    update footer

--- app/_component/footer.tsx
+++ app/_component/footer.tsx
@@ -1,6 +1,6 @@
 export default function Footer() {
   return (
-    <footer className="absolute bottom-8 w-full">
+    <footer className="absolute bottom-8 w-full border-t border-[#888]">
       <p className="text-center">Pied de page</p>
     </footer>
   );
